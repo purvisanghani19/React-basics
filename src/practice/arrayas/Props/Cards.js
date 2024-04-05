5313105033b0f021e3268b669c46acc63cecb5ea
purvi <purvisanghani1@gmail.com>
2024-04-05 15:26:08 +0530

    map method

--- src/practice/arrayas/Props/Cards.js
+++ src/practice/arrayas/Props/Cards.js
@@ -13,12 +13,13 @@ const Cards = () => {
       <h1 className='heading'>
         List of top 5 netflix series
       </h1>
-      <div className='main-card'>
-        <Card img={Sdata[0].img} title={Sdata[0].title} link={Sdata[0].link} />
-        <Card img={Sdata[1].img} title={Sdata[1].title} link={Sdata[1].link} />
-        <Card img={Sdata[2].img} title={Sdata[2].title} link={Sdata[2].link} />
-        <Card img={Sdata[3].img} title={Sdata[3].title} link={Sdata[3].link} />
-      </div>
+      {
+        Sdata.map((val) => {
+          <div className='main-card'>
+            <Card img={val.img} title={val.title} link={val.link} />
+          </div>
+        })
+      }
 
     </>
   )
