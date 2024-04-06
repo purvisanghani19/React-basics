8a571e2f550683f8aec7d5d11365e80084857a98
purvi <purvisanghani1@gmail.com>
2024-04-06 11:09:42 +0530

    time

--- src/practice/Hooks/Increment.js
+++ src/practice/Hooks/Increment.js
@@ -3,19 +3,37 @@ import '../miniproject.css';
 
 const Increment = () => {
 
+    let ct = new Date().toLocaleTimeString();
+
     const [stat, setStat] = useState(0);
+    const [time, setTime] = useState(ct);
+
+
 
     const clickinc = () => {
         setStat(stat + 1);
     };
 
+    const clicktime = () => {
+        ct= new Date().toLocaleTimeString();
+        setTime(ct);
+    }
+
 
     return (
         <>
-            <div className='mini-div' style={{flexDirection :"column"}}>
-                <h1>{stat}</h1>
-                <button className="btn btn-primary" onClick={clickinc}>Go somewhere</button>
+
+            <div className="container">
+                <div className="section">
+                    <h1>{stat}</h1>
+                    <button className="bt-in" onClick={clickinc}>Go somewhere</button>
+                </div>
+                <div className="section">
+                    <h1>{time}</h1>
+                    <button className="bt-in" onClick={clicktime}>Get time</button>
+                </div>
             </div>
+
         </>
     )
 
